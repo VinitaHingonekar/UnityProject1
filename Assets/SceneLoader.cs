a9a0fe2c1864bb299f8a886530338f7c2fa2d8e3
Vinita Hingonekar <hingonekarvinita@gmail.com>
2024-10-29 21:59:22 +0530

    UI Assignment

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene1()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+    
+    public void LoadScene2()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+
+    public void LoadScene3()
+    {
+        SceneManager.LoadScene("Scene3");
+    }
+
+    public void LoadMasterScene()
+    {
+        SceneManager.LoadScene("MasterScene");
+    }
+}
+
